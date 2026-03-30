aa5d0a49365e6e1eb289b290b2f79b04ac3ce631
Colm <colm@stratait.ie>
2026-03-30 17:45:31 +0100

    Add test projects for E2E, integration, and unit testing for the api

---
+++ clients/FlexTradem.Api/tests/FlexTradem.Api.E2ETests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace FlexTradem.Api.E2ETests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ clients/FlexTradem.Api/tests/FlexTradem.Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace FlexTradem.Api.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
